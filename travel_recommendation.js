ebf0f939f8142b75b98739d8c2c4903072268660
evanburton77 <evanburton191@gmail.com>
2025-04-26 17:27:32 -0400

    done needs couple pictures

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -8,14 +8,33 @@ function showDetails() {
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-        const detail = data.details.find(item => item.name.toLowerCase() === input)
-        if (detail){
-            const details = detail.description;
+        const detail = data.countries.find(item => item.name.toLowerCase() === input);
+ if(input === "temples"){
+        const temple = data.temples;
+        resultDiv.innerHTML += `<h1>${temple[1].name}</h1>`
+        resultDiv.innerHTML += `<p>${temple[1].description}</p>` 
+        resultDiv.innerHTML += `<img width="50%" src="${temple[1].imageUrl}">`
+        resultDiv.innerHTML += `<h1>${temple[0].name}</h1>`
+        resultDiv.innerHTML += `<p>${temple[0].description}</p>` 
+        resultDiv.innerHTML += `<img src="${temple[0].imageUrl}">`
+    }   else if(input === "beaches"){
+        const beach = data.beaches; 
+        resultDiv.innerHTML += `<h1>${beach[1].name}</h1>`
+        resultDiv.innerHTML += `<p>${beach[1].description}</p>` 
+        resultDiv.innerHTML += `<img width="50%" src="${beach[1].imageUrl}">`
+        resultDiv.innerHTML += `<h1>${beach[0].name}</h1>`
+        resultDiv.innerHTML += `<p>${beach[0].description}</p>` 
+        resultDiv.innerHTML += `<img src="${beach[0].imageUrl}">`
+    } else if(detail){
+        resultDiv.innerHTML += `<h1>${detail.cities[1].name}</h1>`
+        resultDiv.innerHTML += `<p>${detail.cities[1].description}</p>` 
+        resultDiv.innerHTML += `<img width="50%" src="${detail.cities[1].imageUrl}">`
+        resultDiv.innerHTML += `<h1>${detail.cities[0].name}</h1>`
+        resultDiv.innerHTML += `<p>${detail.cities[0].description}</p>` 
+        resultDiv.innerHTML += `<img src="${detail.cities[0].imageUrl}">`
+    }
 
-            resultDiv.innerHTML += `<h1>${details}`
-        } else {
-            resultDiv.innerHTML = 'destination not found.'
-        }
+        
     })
     .catch(error=> {
         console.error('error', error);
@@ -25,4 +44,4 @@ function showDetails() {
     
 }
 
-btnSearch.addEventListener('click', searchInput);
+btnSearch.addEventListener('click', showDetails);
